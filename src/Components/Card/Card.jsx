c9eb82430d9d33bc8a71767adbdebe9753aef893
RickyAnchores <ricky23anchores@gmail.com>
2023-06-13 21:40:33 +0100

    added card

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({image,title}) => {
+  return (
+    <div className='Card bg-gray-800 text-teal-600 rounded mb-5'>
+        <img src={image} alt="image" />
+        <h3>Title:{title}</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, corporis!</p>
+        <button className="btn">View</button>
+    </div>
+  )
+}
+
+export default Card;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Card from '../../Components/Card/Card'
 
 const Home = () => {
   return (
-    <div className='Home flex justify-center h-5 bg-slate-500'>
+    <div className='Home justify-center bg-slate-500 p-5'>
         <h3>Home</h3>
+        <Card />
+        <Card />
+        <Card />
     </div>
   )
 }
